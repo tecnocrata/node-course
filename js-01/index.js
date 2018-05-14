0824fa7625531ea2fc2437749d6a486b7bae33b2
enrique.ortuno <enrique@ortuno.net>
2018-05-13 21:38:59 -0400

    Adding simple undefined data type sample

--- js-01/index.js
+++ js-01/index.js
@@ -6,8 +6,15 @@ console.log(typeof 3.02);
 console.log(typeof 0.02);
 console.log(typeof "3.02");
 
-console.log(typeof null);
+let x = 'hello';
+console.log(typeof x);
+
+let z;
+console.log(typeof z);
 console.log(typeof undefined);
+
+console.log(typeof null);
+
 
 //Please refer to http://exploringjs.com/es6/ch_symbols.html
 console.log(typeof Symbol('Red'));
