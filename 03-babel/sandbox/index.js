2f7206a52a58e4cfaa09e1c1033e12bf843453b7
enrique.ortuno <enrique@ortuno.net>
2018-06-05 22:21:39 -0400

    First Babel samples

---
+++ 03-babel/sandbox/index.js
@@ -0,0 +1,20 @@
+
+class Person {
+    constructor (name){
+        this.name = name;
+    }
+}
+function* jsRocksIsAwesome() {
+  yield "JS Rocks is Awesome";
+  yield "JS Rocks says JavaScript Rocks";
+  return "because JavaScript really rocks";
+}
+
+var jsRocks = jsRocksIsAwesome();
+
+console.log(jsRocks.next());
+console.log(jsRocks.next());
+console.log(jsRocks.next());
+
+var enrique = new Person('Enrique');
+console.log (enrique.name);
