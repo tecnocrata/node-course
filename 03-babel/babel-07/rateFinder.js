9eb64c59420dcde219a5351da5c09a2badcad304
enrique.ortuno <enrique@ortuno.net>
2018-06-10 22:09:33 -0400

    Adding promise samples and exercises

--- 03-babel/babel-07/rateFinder.js
+++ 03-babel/babel-07/rateFinder.js
@@ -1,7 +1,7 @@
-let url = "rates.json";
-    
-fetch(url)
-    .then(response => response.json())
+let file = "rates.json";
+
+//Please complete this file in order to read rates.json and build a html response
+....(file)
     .then(rates => {
       let html = '';
       rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
