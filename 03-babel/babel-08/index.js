c2f5532921440ea04f5f84372c52033d58684cea
enrique.ortuno <enrique@ortuno.net>
2018-06-11 20:50:40 -0400

    Reorganizing folders and adding classes samples

--- 03-babel/babel-08/index.js
+++ 03-babel/babel-08/index.js
@@ -1,12 +1,42 @@
-import fetch from 'node-fetch';
+//Classes - static members
 
-//Please fix this file, contains errors
+class Rectangle {
+    constructor() {
+        this.width = 4;
+    }
 
-fetch('https://github.com/tecnocrata')
-    .then(res => {
-        console.log(res.ok);
-        console.log(res.status);
-        console.log(res.statusText);
-        console.log(res.headers.raw());
-        console.log(res.headers.get('content-type'));
-    });
+    //static myStaticValue =0;
+}
+
+Rectangle.myStaticValue = 100;
+
+let r1 = new Rectangle();
+console.log(Rectangle.myStaticValue);
+let r2 = new Rectangle();
+console.log(Rectangle.myStaticValue);
+
+class Triple {
+    static triple(n) {
+        if (n === undefined) {
+            n = 1;
+        }
+        return n * 3;
+    }
+}
+
+class BiggerTriple extends Triple {
+    static triple(n) {
+        return super.triple(n) * super.triple(n);
+    }
+}
+
+console.log(Triple.triple()); // 3
+console.log(Triple.triple(6)); // 18
+
+var tp = new Triple();
+
+console.log(BiggerTriple.triple(3));
+// 81 (not affected by parent's instantiation)
+
+console.log(tp.triple());
+// 'tp.triple is not a function'.
