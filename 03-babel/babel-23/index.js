5114cfec36baa8028b69f7c9103726fa538364d5
enrique.ortuno <enrique@ortuno.net>
2018-07-15 19:44:37 -0400

    Fixing comparision operation

--- 03-babel/babel-23/index.js
+++ 03-babel/babel-23/index.js
@@ -6,7 +6,7 @@ getText = function(url, callback) // How can I use this callback?
     let request = new XMLHttpRequest();
     request.onreadystatechange = function()
     {
-        if (request.readyState == 4 && request.status == 200)
+        if (request.readyState === 4 && request.status === 200)
         {
             callback(request.responseText); // Another callback here
         }
