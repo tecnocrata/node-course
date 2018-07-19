c248c2d25a9d6dd68c700e863459afa2a006a58b
enrique.ortuno <enrique@ortuno.net>
2018-07-18 21:26:50 -0400

    Completing grunt sample

--- 05-task-runners&module-bundlers/01-grunt/Gruntfile.js
+++ 05-task-runners&module-bundlers/01-grunt/Gruntfile.js
@@ -1,17 +1,43 @@
-require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
-
-grunt.initConfig({
-	babel: {
-		options: {
-			sourceMap: true,
-			presets: ['env']
-		},
-		dist: {
-			files: {
-				'dist/**/*.js': 'src/app.js'
-			}
-		}
+"use strict";
+module.exports = function(grunt) {
+  grunt.initConfig({
+    babel: {
+      compile: {
+        options: {
+          sourceMap: true,
+          presets: ["env"],
+        },
+        files: [{
+			expand: true,
+			//cwd: '<%= yeoman.server %>',
+			src: [
+			  'src/**/*.js'
+			],
+			dest: 'dist'
+		  }],
+      },
+    },
+    nodeunit: {
+      tasks: ["test/test.js"],
+    },
+    clean: {
+      test: ["dist/**"],
+	},
+	copy: {
+		main: {
+			expand: true,
+			//cwd: '<%= yeoman.client %>',
+			dest: 'dist/',
+			src: ['src/**']
+		  }
 	}
-});
+  });
 
-grunt.registerTask('dev', ['babel']);
+  //grunt.loadTasks("tasks");
+  grunt.loadNpmTasks("grunt-babel");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-nodeunit");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  //grunt.registerTask("build", ["clean", "babel", "nodeunit", "clean"]);
+  grunt.registerTask("build", ["clean", "copy", "babel"]);
+};
