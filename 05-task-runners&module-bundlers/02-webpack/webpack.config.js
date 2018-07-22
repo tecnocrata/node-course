246972bf05b96b4c168f4e869f9629f45b10cb34
enrique.ortuno <enrique@ortuno.net>
2018-07-21 23:44:30 -0400

    Packing well and babeliying well also but not assets and static files were not copied

--- 05-task-runners&module-bundlers/02-webpack/webpack.config.js
+++ 05-task-runners&module-bundlers/02-webpack/webpack.config.js
@@ -4,20 +4,31 @@ var fs = require('fs');
 
 var nodeModules = {};
 fs.readdirSync('node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1;
-  })
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-  });
+    .filter(function (x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function (mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
 
 module.exports = {
-  entry: './src/bin/www',
-  target: 'node',
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'backend.js'
-  },
-  externals: nodeModules,
-  devtool: 'sourcemap'
+    entry: './src/bin/www',
+    target: 'node',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'backend.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env']
+                }
+            }
+        ]
+    },
+    externals: nodeModules,
+    devtool: 'sourcemap'
 }
