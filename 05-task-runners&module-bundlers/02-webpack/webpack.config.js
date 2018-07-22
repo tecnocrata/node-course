1ff0df24f3938a5f7e54ab63c5071268de58027a
enrique.ortuno <enrique@ortuno.net>
2018-07-22 00:38:02 -0400

    Fixing bundling error with webpack

--- 05-task-runners&module-bundlers/02-webpack/webpack.config.js
+++ 05-task-runners&module-bundlers/02-webpack/webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var nodeModules = {};
 fs.readdirSync('node_modules')
@@ -14,8 +15,11 @@ fs.readdirSync('node_modules')
 module.exports = {
     entry: './src/bin/www',
     target: 'node',
+    node: {
+        __dirname: true
+      },
     output: {
-        path: path.join(__dirname, 'build'),
+        path: path.join(__dirname, 'dist'),
         filename: 'backend.js'
     },
     module: {
@@ -29,6 +33,18 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: path.resolve(__dirname, 'src/public'),
+            to: 'src/public'
+          },
+          {
+            from: path.resolve(__dirname, 'src/views'),
+            to: 'src/views'
+          }
+        ])
+  
+    ],
     externals: nodeModules,
     devtool: 'sourcemap'
 }
