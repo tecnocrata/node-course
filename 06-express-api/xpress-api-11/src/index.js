e258b2b77c5ae252e17c1b90b87a5b5c5941834a
enrique.ortuno <enrique@ortuno.net>
2018-08-08 22:18:07 -0400

    Creating reducer

--- 06-express-api/xpress-api-11/src/index.js
+++ 06-express-api/xpress-api-11/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
+import postReducer from './reducers/postReducer';
+
+const store = createStore(postReducer);
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
