609da9a045b873b319d814cccbf466ad6d05c1e7
enrique.ortuno <enrique@ortuno.net>
2018-08-08 22:25:31 -0400

    Enabling redux and creating form

--- 06-express-api/xpress-api-11/src/index.js
+++ 06-express-api/xpress-api-11/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 
 import postReducer from './reducers/postReducer';
-
 const store = createStore(postReducer);
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
