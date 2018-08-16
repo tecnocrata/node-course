334ec03f6636b601cfc98d24b7b430528a02372f
Enrique <enrique@Enriques-iMac.ortuno.lan>
2018-08-15 23:44:28 -0400

    React without redux for get and create

--- 06-express-api/xpress-api-12/src/index.js
+++ 06-express-api/xpress-api-12/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import AppState from './AppState';
+//import { createStore } from 'redux';
+//import { Provider } from 'react-redux';
 
 
 import flightReducer from './reducers/flightReducer';
-const store = createStore(flightReducer);
+//const store = createStore(flightReducer);
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+    <AppState>
+    <App />
+  </AppState>, document.getElementById('root'));
