243af0a48d004484a85ff134805af87351433e45
enroque.ortuno <enrique@ortuno.net>
2018-08-27 21:58:07 -0400

    Fixing parameters except safe

--- 08-mongodb/mongodb-01/index.js
+++ 08-mongodb/mongodb-01/index.js
@@ -4,44 +4,40 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var cors = require('cors');                 // Enable Cors
+var express = require('express'); // call express
+var app = express(); // define our app using express
+var cors = require('cors'); // Enable Cors
 var bodyParser = require('body-parser');
 var mongoContext = require('./mongo');
 app.use(cors());
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080; // set our port
 
 //Validate mongo connection
 mongoContext.connect({
     uri: process.env.MONGO_URL || 'mongodb://localhost/my-db',
     params: {
-          server: {
-            socketOptions: {
-              connectTimeoutMS: 15000
-            }
-          },
-          db: {
-            safe: true
-          },
-          useNewUrlParser: true
+        connectTimeoutMS: 15000,
+        safe: true, //Not supported, 
+        useNewUrlParser: true
     }
-  }, function(err) {
+}, function (err) {
     console.error(`MongoDB connection error: ${err}`);
     process.exit(-1);
-  });
+});
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     // do logging
     console.log('Something is happening.(middleware)');
     next(); // make sure we go to the next routes and don't stop here
